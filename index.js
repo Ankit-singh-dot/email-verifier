016363d39aaad50bc6aaa9996f69235d19c23ab3
Ankit <ankitrajdihara123@gmail.com>
2025-07-24 14:26:20 +0530

    chnages_a_lot

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.post("/verify", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log(" Server is running on http://localhost:3001");
-});
+// app.listen(3001, () => {
+//   console.log(" Server is running on http://localhost:3001");
+// });
+export default app;
